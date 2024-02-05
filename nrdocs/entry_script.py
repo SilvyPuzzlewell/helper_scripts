aa3429c25ffc98ba0354a7e0de2c6e2c230e9d7f
Ronald Krist <ronald.krist@cesnet.cz>
2024-02-05 10:44:21 +0100

    srcipts

--- nrdocs/entry_script.py
+++ nrdocs/entry_script.py
@@ -1,18 +1,30 @@
 import sys
 import urllib3
+import argparse
+
+from nrdocs import new_with_requests
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
 import importlib
 
-if __name__ == "__main__":
-    module = sys.argv[1]
-    func = sys.argv[2]
+def current_function():
+    return new_with_requests.script
 
-    module = importlib.import_module(f".{module}", package="nrdocs")
-    function_to_call = getattr(module, func)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--module')
+    parser.add_argument('--func')
+
+    if hasattr(parser, "module") and hasattr(parser, "func"):
+        module = parser.module
+        func = parser.func
+        module = importlib.import_module(f".{module}", package="nrdocs")
+        function_to_call = getattr(module, func)
+    else:
+        function_to_call = current_function()
 
     # Call the function if it exists
     if callable(function_to_call):
-        function_to_call(*sys.argv[3:])
+        function_to_call(*sys.argv[1:])
--- nrdocs/new_with_requests.py
+++ nrdocs/new_with_requests.py
@@ -4,21 +4,27 @@ from .repo_requests import create_record, create_request
 from .utils import nrdocs_sample_record, BASE_URL, nrdocs_sample_metadata, authorization_header, ui_serialization_header
 
 
-def script(token):
+def script(creator_token, receiver_token):
     sample_record = {"metadata": nrdocs_sample_metadata(), "files": {"enabled": False}}
     #create record
-    resp_record = create_record(BASE_URL, sample_record, token)
+    resp_record = create_record(BASE_URL, sample_record, creator_token)
     if resp_record.status_code != 201:
         print(f"wrong status code {resp_record.status_code}")
         print(resp_record.text)
     assert resp_record.status_code == 201
     print("record created")
 
-    publish_request = create_request(BASE_URL, token, "publish_draft", "user", "2", "documents_draft", resp_record.json()["id"])
+    publish_request = create_request(BASE_URL, creator_token, "publish_draft", "user", "2", "documents_draft", resp_record.json()["id"])
     assert publish_request.status_code == 201
-    request_detail = requests.get(publish_request.json()['links']['self'], headers=authorization_header(token), verify=False).json()
-    request_detail_ui_serialization = requests.get(publish_request.json()['links']['self'], headers=authorization_header(token) | ui_serialization_header(), verify=False).json()
+    request_detail = requests.get(publish_request.json()['links']['self'], headers=authorization_header(creator_token), verify=False).json()
+    request_detail_ui_serialization = requests.get(publish_request.json()['links']['self'], headers=authorization_header(creator_token) | ui_serialization_header(), verify=False).json()
     print("request created")
+    submit_request = requests.post(request_detail['links']['actions']['submit'], headers=authorization_header(creator_token), verify=False)
+    print("request submitted")
+    # TODO get request for receiver from record
+    accept_request = requests.get(f"{BASE_URL}/api/requests/{request_detail['id']}", headers=authorization_header(receiver_token), verify=False).json()
+    accept = requests.post(accept_request['links']['actions']['accept'], headers=authorization_header(receiver_token), verify=False)
+    print("request accepted")
 
     """
     #publish record
