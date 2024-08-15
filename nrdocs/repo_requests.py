78ec7b4e9d0aca35ecaf4278f04a38cfb5a9bc02
Ronald Krist <ronald.krist@cesnet.cz>
2024-08-15 08:49:12 +0200

    update

--- nrdocs/repo_requests.py
+++ nrdocs/repo_requests.py
@@ -7,7 +7,7 @@ def create_record(base_url, data, token, repo="docs"):
     return requests.post(url=f'{base_url}/api/{repo}', headers=authorization_header(token), json=data, verify=False)
 
 def create_record_in_community(base_url, data, token, community_id, repo="docs"):
-    return requests.post(f"{base_url}/api/communities/{community_id}/{repo}/records", headers=authorization_header(token), json=data, verify=False)
+    return requests.post(f"{base_url}/api/communities/{community_id}/{repo}", headers=authorization_header(token), json=data, verify=False)
 
 def create_request(base_url, token, request_type, topic_type, topic_id):
     data = {
--- nrdocs/workflows.py
+++ nrdocs/workflows.py
@@ -1,6 +1,5 @@
 import time
 import requests
-from invenio_requests.records.api import Request
 from nrdocs.repo_requests import create_record, create_record_in_community
 from nrdocs.utils import authorization_header, BASE_URL, nrdocs_sample_metadata, _find_request_from_search_by_type, \
     create_request_on_record, upload_file, _find_request_by_type_id
@@ -41,7 +40,7 @@ def invite(user_id, community_id, role, owner_token, reader_token):
     """Add/invite a user to a community with a specific role."""
     invitation_data = {
         "members": [{"type": "user", "id": user_id}],
-        "role": "reader",
+        "role": role,
         "message": "Welcome to the club!",
 
     }
@@ -94,14 +93,17 @@ def create_community(creator_token, community_dict=None):
         comm = requests.post(f"{BASE_URL}/api/communities", headers=authorization_header(creator_token), json=community_dict, verify=False).json()
     else:
         comm = existing_comm
+    comm = comm | {"custom_fields": {"workflow": "default"}}
+    result = requests.put(f"{BASE_URL}/api/communities/{comm['id']}",
+                  headers=authorization_header(creator_token), json=comm, verify=False)
     return comm
 
 def invite_reader():
     """"""
 
 def do_request(receiver_token, creator_token, record_id, type, payload=None, is_draft=False, create_forbidden=False):
-    url = f"{BASE_URL}/api/docs/{record_id}/draft?expand=true" if is_draft else f"{BASE_URL}/api/docs/{record_id}?expand=true"
-    record_resp = requests.get(url, headers=authorization_header(creator_token), verify=False)
+    record_url = f"{BASE_URL}/api/docs/{record_id}/draft?expand=true" if is_draft else f"{BASE_URL}/api/docs/{record_id}?expand=true"
+    record_resp = requests.get(record_url, headers=authorization_header(creator_token), verify=False)
     #todo CommunityAlreadyIncludedException should throw 500 (400 instead)??
     if create_forbidden:
         request = _find_request_by_type_id(record_resp.json()["expanded"]["request_types"], type)
@@ -118,74 +120,88 @@ def do_request(receiver_token, creator_token, record_id, type, payload=None, is_
                            headers=authorization_header(receiver_token), verify=False)
     assert accept.status_code == 200
 
-def change_workflow_test(sample_record, token, community_id):
-    result = requests.post(f"{BASE_URL}/api/communities/{community_id}/workflow/lalala", headers=authorization_header(token), verify=False)
+def change_workflow_test(token, community_id, community_data):
+    result_default = requests.put(f"{BASE_URL}/api/communities/{community_id}", headers=authorization_header(token), json=community_data|{"custom_fields": {"workflow": "default"}}, verify=False)
+    result_nonexistent = requests.put(f"{BASE_URL}/api/communities/{community_id}", headers=authorization_header(token),
+                          json=community_data | {"custom_fields": {"workflow": "nonexistent"}}, verify=False)
     # todo handling nonexisting workflows, endpoint to get workflow
-    non_existing_workflow_create = create_record_in_community(BASE_URL, sample_record, token, community_id, repo="docs")
-    assert non_existing_workflow_create.status_code == 500
-    backchange = requests.post(f"{BASE_URL}/api/communities/{community_id}/workflow/default", headers=authorization_header(token), verify=False)
-    r = create_record_in_community(BASE_URL, sample_record, token, community_id, repo="docs")
-    assert r.status_code == 201
+    #non_existing_workflow_create = create_record_in_community(BASE_URL, sample_record, token, community_id, repo="docs")
+    #assert non_existing_workflow_create.status_code == 400
+    #backchange = requests.put(f"{BASE_URL}/api/communities/{community_id}", headers=authorization_header(token), json=community_data["data"]|{"custom_fields": {"workflow": "default"}}, verify=False)
+    #r = create_record_in_community(BASE_URL, sample_record, token, community_id, repo="docs")
+    assert result_default.status_code == 200
+    assert result_nonexistent.status_code == 400
     return
 
-def script(owner_token, reader_token):
+def script(owner_token, curator_token, *args, **kwargs):
     community_id = create_community(owner_token)["id"]
-    invite('2', community_id, "reader", owner_token, reader_token)
+    invite('2', community_id, "curator", owner_token, curator_token)
     community2_id =create_community(owner_token, community_dict=COMMUNITY_DICT2)["id"]
-    invite('2', community2_id, "reader", owner_token, reader_token)
+    invite('2', community2_id, "curator", owner_token, curator_token)
     community3_id =create_community(owner_token, community_dict=COMMUNITY_DICT3)["id"]
-    invite('2', community3_id, "reader", owner_token, reader_token)
+    invite('2', community3_id, "curator", owner_token, curator_token)
 
     sample_record = {"metadata": nrdocs_sample_metadata()}
 
+    # create is temporarily auth user
     # test direct create disabled
     resp_record = create_record(BASE_URL, sample_record, owner_token)
-    assert resp_record.status_code == 403
+    assert resp_record.status_code == 400
+    # test direct create in community allowed
+    resp_record = create_record(BASE_URL, sample_record|{"parent":{"communities": {"default": community_id}}}, owner_token)
+    assert resp_record.status_code == 201
 
-    change_workflow_test(sample_record, reader_token, community_id)
-    resp_record = create_record_in_community(BASE_URL, sample_record, reader_token, community_id, repo="docs")
+    change_workflow_test(owner_token, community_id, requests.get(f"{BASE_URL}/api/communities/{community_id}", headers=authorization_header(owner_token), verify=False).json())
+    resp_record = create_record_in_community(BASE_URL, sample_record, token=curator_token, community_id=community_id, repo="docs")
+    assert resp_record.status_code == 201
+    do_request(receiver_token=curator_token, creator_token=owner_token, record_id=resp_record.json()["id"], type='publish_draft', is_draft=True, create_forbidden=True)
 
+    resp_record = create_record_in_community(BASE_URL, sample_record, token=owner_token, community_id=community_id, repo="docs")
     record_id = resp_record.json()["id"]
     upload_file(resp_record.json(), owner_token)
     time.sleep(2)
-    do_request(reader_token, owner_token, record_id, 'publish_draft', is_draft=True, create_forbidden=True) #only record owners should be able to create according to workflow
-    do_request(owner_token, reader_token, record_id, 'publish_draft', is_draft=True)
-
+    #only owner is also record owner and curator is allowed
+    do_request(receiver_token=curator_token, creator_token=owner_token, record_id=record_id, type='publish_draft', is_draft=True)
+    """
     #test submit secondary
     time.sleep(2)
     payload = {'payload': {'community': community2_id}}
-    do_request(owner_token, reader_token, record_id, "secondary_community_submission", payload)
+    do_request(owner_token, curator_token, record_id, "secondary_community_submission", payload)
 
     #test migrate
     payload = {'payload': {'community': community3_id}}
-    do_request(owner_token, reader_token, record_id, "community_migration", payload)
+    do_request(owner_token, curator_token, record_id, "community_migration", payload)
 
     time.sleep(2)
-    record = requests.get(f"{BASE_URL}/api/docs/{record_id}", headers=authorization_header(reader_token), verify=False).json()
+    record = requests.get(f"{BASE_URL}/api/docs/{record_id}", headers=authorization_header(curator_token), verify=False).json()
     record_communities = record["parent"]["communities"]
     assert set(record_communities["ids"]) == {community2_id, community3_id}
     assert record_communities["default"] == community3_id
 
     # todo global search service bug - thesis service added twice from eps!!
-    record_communities_search = requests.get(f"{BASE_URL}/api/docs/{record_id}/communities", headers=authorization_header(reader_token), verify=False)
-    community_records = requests.get(f"{BASE_URL}/api/communities/{community_id}/records", headers=authorization_header(reader_token), verify=False)
+    # should be docs instead of documents i guess
+    record_communities_search = requests.get(f"{BASE_URL}/api/documents/{record_id}/communities", headers=authorization_header(curator_token), verify=False)
+    community_records = requests.get(f"{BASE_URL}/api/communities/{community_id}/records", headers=authorization_header(curator_token), verify=False)
+    assert record_communities_search.status_code == 200
+    assert len(record_communities_search.json()["hits"]["hits"]) == 2
+    assert community_records.status_code == 200
 
     #test remove secondary
     payload = {'payload': {'community': community2_id}}
-    do_request(owner_token, reader_token, record_id, "remove_secondary_community", payload)
+    do_request(owner_token, curator_token, record_id, "remove_secondary_community", payload)
 
     time.sleep(2)
-    record = requests.get(f"{BASE_URL}/api/docs/{record_id}", headers=authorization_header(reader_token), verify=False).json()
+    record = requests.get(f"{BASE_URL}/api/docs/{record_id}", headers=authorization_header(curator_token), verify=False).json()
     record_communities = record["parent"]["communities"]
     assert set(record_communities["ids"]) == {community3_id}
     # test edit autoapprove
 
-
+    """
     #test delete record
     delete_directly = requests.delete(f"{BASE_URL}/api/docs/{record_id}", headers=authorization_header(owner_token), verify=False)
     assert delete_directly.status_code == 403
 
-    do_request(owner_token, reader_token, record_id, "delete_published_record")
+    do_request(receiver_token=curator_token, creator_token=owner_token, record_id=record_id, type='delete_published_record') # todo implement access field to not crash IfRestricted generator
     time.sleep(2)
-    record_resp = requests.get(f"{BASE_URL}/api/docs/{record_id}", headers=authorization_header(reader_token), verify=False)
+    record_resp = requests.get(f"{BASE_URL}/api/docs/{record_id}", headers=authorization_header(curator_token), verify=False)
     assert record_resp.status_code == 410
