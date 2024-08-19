1f68c806a95e87df4c74f793da27b6e6fc83ac2a
Ronald Krist <ronald.krist@cesnet.cz>
2024-08-19 13:24:55 +0200

    savepoint

--- sandbox.sh
+++ sandbox.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
+set -e
+if [ $# -lt 1 ]; then
+    VENV_PATH=/home/ron/prace/nr-docs/.venv/bin/activate
+else
+    VENV_PATH=/home/ron/prace/$1/.venv/bin/activate
+fi
+SCRIPT_PATH=$(realpath "$0")
+SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
 
-myArray=("cat" "dog" "mouse" "frog")
-for str in ${myArray[@]}; do
-  echo $str
+. $VENV_PATH
+cd $SCRIPT_DIR
+
+for file in "$(pwd)"/current_token_*; do
+  if [ -e "$file" ]; then
+    echo "Deleting: $file"
+  fi
 done
